a336f4944e1afa8359e760c8ad1780398cee124b
Daniel Soutchkov <daniel.soutchkov@learnet.se>
2024-10-17 15:23:37 +0200

    test2

--- Minröjare.py
+++ Minröjare.py
@@ -3,14 +3,12 @@ class Square:
         self.x = x
         self.y = y
     
-
-
 class Number(Square):
     def __init__(self, x, y):
         super().__init__(x, y)
     
-
 class Bomb(Square):
     def __init__(self, x, y):
         super().__init__(x, y)
     
+x = "hello"
