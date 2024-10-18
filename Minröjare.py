0f92ede1ea44beea03feafd0dfabd36696cbaa59
Jonatan <jonatan.lantz.se@gmail.com>
2024-10-18 15:15:11 +0200

    added makeGrid(), displayGrid(), and __str__ to the Square class.

--- Minröjare.py
+++ Minröjare.py
@@ -2,12 +2,27 @@ class Square:
     def __init__(self,x,y):
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
     
 class Number(Square):
     def __init__(self, x, y):
         super().__init__(x, y)
     
-
 class Bomb(Square):
     def __init__(self, x, y):
         super().__init__(x, y)
+
+def makeGrid(width, height):
+    return [[Square(x, y) for x in range(height)] for y in range(width)]
+
+def displayGrid(grid):
+    for x in grid:
+        print('\n')
+        for y in x:
+            print(y, end=' ')
+
+grid = makeGrid(5, 5)
+
+displayGrid(grid)
