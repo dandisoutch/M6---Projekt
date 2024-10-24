3346e8277cbf7b5c2f8513f84a0b759cbb04eb42
Jonatan <jonatan.lantz.se@gmail.com>
2024-10-24 12:07:56 +0200

    Created countBombs in Squareclass. Also copied over some code from the other branch :)

--- Minröjare.py
+++ Minröjare.py
@@ -7,21 +7,23 @@ class Square:
         self.bomb = bomb
         self.flag = flag
         self.view = view
+        self.bombCount = 0
 
-
+    def countBombs(self, grid): #Counts the bombs around the square with given grid
+        amount = 0
+        if grid[gameHeight-self.y][self.x-1].bomb == True: #return 0 if square contains a bomb
+            return 0
+        else: #Counts bombs
+            for row in grid[max(gameHeight-self.y-1, 0):min(gameHeight-self.y+2, 5)]:
+                for column in row[max(self.x-2, 0):min(self.x+1, 5)]:
+                    if column.bomb == True:
+                        amount += 1
+        return amount
+            
     def __str__(self):
         if self.bomb == True:
             return 'X    '
-        else: return 'O    '
-        # return f'({self.x}, {self.y}, {self.bomb})'
-    
-# class Number(Square):
-#     def __init__(self, x, y):
-#         super().__init__(x, y)
-    
-# class Bomb(Square):
-#     def __init__(self, x, y):
-#         super().__init__(x, y)
+        else: return f'{self.bombCount}    '
 
 #The game's height and width.
 gameWidth = 5
@@ -29,7 +31,7 @@ gameHeight = 5
 
 running = True
 
-bombPerRow = gameWidth/3
+bombPerRow = gameWidth // 3
 
 def makeGrid(width, height): #makes a grid with square objects, each with their own coordinate.
     return [[Square(x+1, height-y) for x in range(width)] for y in range(height)]
@@ -54,11 +56,12 @@ def plantBombs(grid): #Plants bombs in selected grid.
                 bombCount += 1
             visited.append(square.x)
 
+def calculateBombCounts(grid):
+    for y in grid:
+        for x in y:
+            x.bombCount = x.countBombs(grid)
+
 playGrid = makeGrid(gameWidth, gameHeight)
 plantBombs(playGrid)
+calculateBombCounts(playGrid)  # Calculate bomb counts for each square
 displayGrid(playGrid)
-
-# while running == True:
-#     userInput = input("select your square: ")
-#     if userInput == "quit":
-#         running = False
