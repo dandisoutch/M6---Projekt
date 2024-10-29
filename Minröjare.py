048d955a5dbaaac66fa142a846c962f728c21ff0
Jonatan <jonatan.lantz.se@gmail.com>
2024-10-29 15:12:57 +0100

    Changed y-axle. Now goes down.

--- Minröjare.py
+++ Minröjare.py
@@ -11,25 +11,31 @@ class Square:
 
     def countBombs(self, grid, width, height): #Counts the bombs around the square with given grid
         amount = 0
-        if grid[height-self.y][self.x-1].bomb == True: #return 0 if square contains a bomb
+        y = self.y-1
+        x = self.x-1
+        if grid[y][x].bomb == True: #return 0 if square contains a bomb
             return 0
         else: #Counts bombs
-            for row in grid[max(height-self.y-1, 0):min(height-self.y+2, height)]:
-                for column in row[max(self.x-2, 0):min(self.x+1, width)]:
+            for row in grid[max(y-1, 0):min(y+2, height)]:
+                for column in row[max(x-1, 0):min(x+2, width)]:
                     if column.bomb == True:
                         amount += 1
         return amount
 
     def emptySquares(self, grid, width, height):
-        grid[height-self.y][self.x-1].view = True
-        if grid[height-self.y][self.x-1].bombCount == 0 and grid[height-self.y][self.x-1].bomb == False:
-            for row in grid[max(height-self.y-1, 0):min(height-self.y+2, height)]:
-                for column in row[max(self.x-2, 0):min(self.x+1, width)]:
+        y = self.y-1
+        x = self.x-1
+        grid[y][x].view = True
+        if grid[y][x].bombCount == 0 and grid[y][x].bomb == False:
+            for row in grid[max(y-1, 0):min(y+2, height)]:
+                for column in row[max(x-1, 0):min(x+2, width)]:
                     if column.view == False and column.bomb == False:
                         column.emptySquares(grid, width, height)
                         
             
     def __str__(self):
+        # return f'({self.x}, {self.y})'
+
         if self.flag and not self.view:
             return 'F    '
         else:
@@ -50,16 +56,16 @@ running = True
 bombPerRow = gameWidth // 4
 
 def makeGrid(width, height): #makes a grid with square objects, each with their own coordinate.
-    return [[Square(x+1, height-y) for x in range(width)] for y in range(height)]
+    return [[Square(x+1, y+1) for x in range(width)] for y in range(height)]
 
 def displayGrid(grid): #Displays a given grid. Also prints where the X and Y axis is.
     print(f'x-values ------>', end = '')
     print('\n')
-    for x in grid: #looks through all rows (x-values)
-        for y in x: #looks through the columns in the rows (y-values)
-            print(y, end=' ') #prints out the squareObject (calls squares __str__ to show its x, y)
-        if x[-1].y == 1:
-            print('^ y-values ^')
+    for y in grid: #looks through all rows (x-values)
+        for x in y: #looks through the columns in the rows (y-values)
+            print(x, end=' ') #prints out the squareObject (calls squares __str__ to show its x, y)
+        if y[-1].y == 1:
+            print('v y-values v', end= '')
         print('\n')
 
 
@@ -87,6 +93,7 @@ displayGrid(playGrid)
 # Quirky algorithm:
 
 
+
 while True:
     option = input("Type F for flag or Q to quit: ").lower()
 
@@ -94,7 +101,7 @@ while True:
         break
 
     try:
-        y = gameHeight - int(input("\ny: "))
+        y = int(input("\ny: ")) -1
         x = int(input("x: ")) -1
         
         if option == "f":
