31b0a14746e1367fa2e13592884c10986de3589b
Jonatan <jonatan.lantz.se@gmail.com>
2024-10-31 10:59:15 +0100

    added comments and removed unneccecary lines from plantBombs.

--- Minröjare.py
+++ Minröjare.py
@@ -11,7 +11,7 @@ class Square:
 
     def countBombs(self, grid, width, height): #Counts the bombs around the square with given grid
         amount = 0
-        y = self.y-1 #Translates x-y values to use them in grid[][]
+        y = self.y-1 #Translates x-y values to use them in grid
         x = self.x-1
         if grid[y][x].bomb == True: #return 0 if square contains a bomb
             return 0
@@ -21,7 +21,10 @@ class Square:
                     if column.bomb == True:
                         amount += 1
         return amount
-
+    
+    #This function checks for empty squares (squares with 0 bombcount)
+    #around the selected square, and calls upon the same function on those squares.
+    #This reveals big areas if there's a lot of empty squares.
     def emptySquares(self, grid, width, height):
         y = self.y-1
         x = self.x-1
@@ -34,8 +37,7 @@ class Square:
                         
             
     def __str__(self):
-        # return f'({self.x}, {self.y})'
-
+        #This function is what a square will show if you print it out.
         if self.flag and not self.view:
             return 'F    '
         else:
@@ -47,13 +49,12 @@ class Square:
                 return '*    '
 
 #The game's height and width.
-#Make the user choose gamesize?
 gameWidth = 6
 gameHeight = 6
 
 running = True
 
-bombPerRow = gameWidth // 4
+bombPerRow = gameWidth // 4 #how many bombs there are per row.
 
 def makeGrid(width, height): #makes a grid with square objects, each with their own coordinate.
     return [[Square(x+1, y+1) for x in range(width)] for y in range(height)]
@@ -61,37 +62,33 @@ def makeGrid(width, height): #makes a grid with square objects, each with their 
 def displayGrid(grid): #Displays a given grid. Also prints where the X and Y axis is.
     print(f'x-values ------>', end = '')
     print('\n')
-    for y in grid: #looks through all rows (x-values)
-        for x in y: #looks through the columns in the rows (y-values)
-            print(x, end=' ') #prints out the squareObject (calls squares __str__ to show its x, y)
+    for y in grid: #looks through all rows
+        for x in y: #looks through the columns (the squares in the rows)
+            print(x, end=' ') #prints out the squareObject
         if y[-1].y == 1:
             print('v y-values v', end= '')
         print('\n')
 
-
-#Make this more random, still has tendency to spawn bombs to the left more.
-def plantBombs(grid): #Plants bombs in selected grid.
-    for x in grid:
-        visited = []
+def plantBombs(grid): #Randomly plants bombs in selected grid.
+    for y in grid: #looks through rows (list)
         bombCount = 0
         while True:
-            square = x[randint(0, len(x))-1]
-            if square.bomb == False and visited.count(square) != 1:
+            square = y[randint(0, len(y))-1] #randomly selects a square in the row
+            if square.bomb == False: #Check if there's already a bomb there
                 bombCount += 1
-                visited.append(square)
                 square.bomb = True
             if bombCount == bombPerRow:
                 break
 
 
-def calculateBombCounts(grid):
+def calculateBombCounts(grid): #loops through the grid and updates the squares bombcount
     for y in grid:
         for x in y:
             x.bombCount = x.countBombs(grid, gameWidth, gameHeight)
 
 playGrid = makeGrid(gameWidth, gameHeight)
 plantBombs(playGrid)
-calculateBombCounts(playGrid)  # Calculate bomb counts for each square
+calculateBombCounts(playGrid)
 displayGrid(playGrid)
 
 while True:
